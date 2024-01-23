3ed95b13b65c410298e98f8cc4f3feed8b302791
Shreyas Veturi <veturi.s@etoncollege.org.uk>
2024-01-23 12:55:26 +0000

    Initial Commit

---
+++ YummiApp/YummiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  YummiAppApp.swift
+//  YummiApp
+//
+//  Created by Veturi, Shreyas (HJRM) on 23/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct YummiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
