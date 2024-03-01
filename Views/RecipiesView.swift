e40d7f43b33ea83bf9e7e10cfa8af58d535bb0e5
Shreyas Veturi <veturi.s@etoncollege.org.uk>
2024-03-01 10:29:11 +0000

    Creating REcipes view

---
+++ Views/RecipiesView.swift
@@ -0,0 +1,24 @@
+//
+//  RecipiesView.swift
+//  YummiApp
+//
+//  Created by Veturi, Shreyas (HJRM) on 27/02/2024.
+//
+
+import Foundation
+import SwiftUI
+
+struct RecipesView: View {
+    
+    @State var recpies = Recipe.example
+    var body: some View {
+        Text("todo")
+    }
+}
+
+#Preview {
+    RecipesView(recipes: Recipe)
+}
+
+
+
